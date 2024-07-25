ff8c16de3f9da5cd1ca9035ef0487f0a96d9f598
Ethan <ethansimpson285@gmail.com>
2024-07-26 00:12:47 +0100

    update to correct h5 form

--- Adam/HyPER/create_hyper_h5.py
+++ Adam/HyPER/create_hyper_h5.py
@@ -15,23 +15,31 @@ pad_to_jet = 20
 print('opening root')
 root_file   = uproot.open(filepath)['Delphes']
 
-
-njet = root_file['Jet'].array()        
+njet = root_file['Jet'].array()       
 mask_jet = njet>=6      
 njet = njet[mask_jet]         
-jet_pt = root_file['Jet/Jet.PT'].array()[mask_jet]
-jet_eta = root_file['Jet/Jet.Eta'].array()[mask_jet]
-jet_phi = root_file['Jet/Jet.Phi'].array()[mask_jet]     
-jet_m = root_file['Jet/Jet.Mass'].array()[mask_jet]
+jet_pt   = root_file['Jet/Jet.PT'].array()[mask_jet]
+jet_eta  = root_file['Jet/Jet.Eta'].array()[mask_jet]
+jet_phi  = root_file['Jet/Jet.Phi'].array()[mask_jet]     
+jet_m    = root_file['Jet/Jet.Mass'].array()[mask_jet]
 jet_btag = root_file['Jet/Jet.BTag'].array()[mask_jet]
 
-n_particles = root_file['Particle'].array()[mask_jet]
-particle_pid = root_file['Particle/Particle.PID'].array()[mask_jet]
+#Apply cuts of jet_pt > 25GeV and absolute eta < 2.5
+print("applying cuts")
+cuts = np.logical_and(jet_pt > 25 , abs(jet_eta) < 2.5)
+jet_pt   = jet_pt[cuts]
+jet_eta  = jet_eta[cuts]
+jet_phi  = jet_phi[cuts]     
+jet_m    = jet_m[cuts]
+jet_btag = jet_btag[cuts]
+
+n_particles     = root_file['Particle'].array()[mask_jet]
+particle_pid    = root_file['Particle/Particle.PID'].array()[mask_jet]
 particle_status = root_file['Particle/Particle.Status'].array()[mask_jet]
-particle_m = root_file['Particle/Particle.Mass'].array()[mask_jet] 
-particle_pt = root_file['Particle/Particle.PT'].array()[mask_jet] 
-particle_eta = root_file['Particle/Particle.Eta'].array()[mask_jet]  
-particle_phi = root_file['Particle/Particle.Phi'].array()[mask_jet] 
+particle_m      = root_file['Particle/Particle.Mass'].array()[mask_jet] 
+particle_pt     = root_file['Particle/Particle.PT'].array()[mask_jet] 
+particle_eta    = root_file['Particle/Particle.Eta'].array()[mask_jet]  
+particle_phi    = root_file['Particle/Particle.Phi'].array()[mask_jet] 
 
 length = len(n_particles)
 new_values = np.array([1,4,2,3,5,6])
@@ -62,7 +70,9 @@ def quick_method(jets, particles, max_jet, limit = length):
 def expensive_method(jets, particles, max_jet ,limit=length):
     # requires non padded jet array
     limit = min(len(jets), limit)
-    print(max_jet)
+    new_values = np.array([1,4,2,3,5,6])
+
+    # print(max_jet)
     matched = np.zeros((limit,max_jet))
     for event, (event_jets, event_particles) in tqdm(enumerate(zip(jets[:limit], particles[:limit]))):
         all_Rs = np.array([jet.deltaR(event_particles) for jet in event_jets])
@@ -101,39 +111,52 @@ def full_method(jet_vectors, jet_vectors_padded, particle_vectors, max_jet, limi
         return crop(jet_truth_matched, njet[:limit], max_jet)
     return jet_truth_matched
 
+# Building vectors
 print('creating vectors')   
 max_jet = np.max(njet)
-particle_vectors = vector.zip({'pt':particle_pt, 'eta':particle_eta, 'phi':particle_phi, 'm' :particle_m})[particle_status == 23][:,-6:]
+particle_vectors   = vector.zip({'pt':particle_pt, 'eta':particle_eta, 'phi':particle_phi, 'm' :particle_m})[particle_status == 23][:,-6:]
 jet_vectors_padded = vector.zip({'pt':pad_variable(jet_pt, max_jet), 'eta':pad_variable(jet_eta, max_jet, pad_to = 99), 'phi':pad_variable(jet_phi, max_jet), 'm' :pad_variable(jet_m, max_jet)})
-jet_vectors  = vector.zip({'pt':jet_pt, 'eta':jet_eta, 'phi':jet_phi, 'm' :jet_m})
+jet_vectors        = vector.zip({'pt':jet_pt, 'eta':jet_eta, 'phi':jet_phi, 'm' :jet_m})
 
+# Performing truth-matching
 print('truth matching')
 jet_truthmatch = full_method(jet_vectors, jet_vectors_padded, particle_vectors, max_jet)
 
 print('forming data')
-data = {}
-data['njet'] = njet
-data['nbtagged'] = np.sum(jet_btag, axis=1)
-data['allMatchedEvent'] = np.sum(jet_truthmatch, axis=1) == 21
-data['leastOneMatchedW'] = (np.any(jet_truthmatch == 2, axis=1) & np.any(jet_truthmatch == 3, axis=1)) | (np.any(jet_truthmatch == 5, axis=1) & np.any(jet_truthmatch == 6, axis=1))
-data['jet_pt'] = pad_variable(jet_pt, max_jet)
-data['jet_eta'] = pad_variable(jet_eta, max_jet, pad_to = 0)
-data['jet_phi'] = pad_variable(jet_phi, max_jet)
-data['jet_e'] = pad_variable(vector.zip({'pt':jet_pt, 'eta':jet_eta, 'phi':jet_phi, 'm' :jet_m}).e, max_jet)
-data['jet_m'] = pad_variable(jet_m, max_jet)
-data['jet_btag'] = pad_variable(jet_btag, max_jet)
-data['jet_truthmatch'] = jet_truthmatch
-data['particle_pid'] = particle_pid[particle_status == 23][:,-6:]
-data['particle_pt'] = particle_pt[particle_status == 23][:,-6:]
-data['particle_eta'] = particle_eta[particle_status == 23][:,-6:]
-data['particle_phi'] = particle_phi[particle_status == 23][:,-6:]
-data['particle_m'] = particle_m[particle_status == 23][:,-6:]
+
+# Fill global data
+global_dt   = np.dtype([('njet', np.float32), ('nbTagged', np.float32)])
+global_data = np.zeros((len(njet), 1), dtype=global_dt)
+
+njet     = njet.to_numpy().reshape(-1,1)
+nbTagged = np.sum(jet_btag, axis=1).to_numpy().reshape(-1,1)
+
+global_data['njet']     = njet
+global_data['nbTagged'] = nbTagged
+
+# Fill jet data
+node_dt  = np.dtype([('e', np.float32), ('eta', np.float32), ('phi', np.float32), ('pt', np.float32), ('btag', np.float32), ('charge', np.float32), ('id', np.float32)])
+jet_data = np.zeros((len(njet), max_jet), dtype=node_dt)
+
+jet_data['pt']   = pad_variable(jet_pt, max_jet)
+jet_data['eta']  = pad_variable(jet_eta, max_jet, pad_to = 0)
+jet_data['phi']  = pad_variable(jet_phi, max_jet)
+jet_data['e']    = pad_variable(vector.zip({'pt':jet_pt, 'eta':jet_eta, 'phi':jet_phi, 'm' :jet_m}).e, max_jet)
+jet_data['btag'] = pad_variable(jet_btag, max_jet)
+jet_data['charge'] = np.zeros(len(njet)).reshape(-1,1)
+jet_data['id']     = np.zeros(len(njet)).reshape(-1,1)
+
+
+VertexID_data = jet_truthmatch
 
 print('saving data')
 h5_file = h5py.File(outfile, 'w')
-group = h5_file.create_group('Delphes')
-for key, value in data.items():
-    group.create_dataset(key, data=value)
+inputs_group = h5_file.create_group('INPUTS')
+labels_group = h5_file.create_group('LABELS')
+
+inputs_group.create_dataset("jet", data=jet_data)
+inputs_group.create_dataset("global", data=global_data)
+labels_group.create_dataset("VertexID", data=VertexID_data)
 
 h5_file.close()
 print('program finished')
